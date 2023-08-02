ec4986dd9da0e7e2f46870526412b74259a96e1e
Yaros85 <hedgy85@hotmail.com>
2023-08-02 11:59:59 +0300

    DZ1

--- sort.py
+++ sort.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import shutil
+import re
 
 CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
 TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
@@ -8,7 +9,7 @@ TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", 
 TRANS = {}
 
 path_parent = 'C://Users/Yaroslav/OneDrive/Рабочий стол/ToSort/'
-text_file = 'C://Users/Yaroslav/OneDrive/Рабочий стол/ToSort/text_1.txt'
+text_file = 'C://Users/Yaroslav/OneDrive/Рабочий стол/ToSort/data_written.bin'
 
 p = Path(path_parent)
 p_audio = Path(path_parent + '/' + 'audio')
@@ -28,33 +29,40 @@ for c, t in zip(CYRILLIC_SYMBOLS, TRANSLATION):
     #print(TRANS)
 
 def normalize(name):
+    
+    list_names = list()     
     #print(name)
     element = name.translate(TRANS)
+    symbols = re.findall('\W+', element)
+    #print(element)
+    #print(symbols)
+    for i in symbols:
+        element = element.replace(i,'_') 
+        #list_names.append(element)
+        
     return element
 
 
     
 def sort_files_folders(p):
-    
+
     # print(p)
     for i in p.iterdir():
-        
         p_new = ''
         if i.is_dir():
-            print(i.name)
+            #print(i.name)
             p_new = str(i)
-        
             sort_files_folders(Path(p_new))
         
         if i.is_file():  
             name_without_extension = i.stem
             ext = i.suffix
-            #print(ext)
             new_name = normalize(name_without_extension) #передать имя каждого файла
+            #print(new_name)
             if ext == '.jpeg' or ext == '.png' or ext == '.jpg' or ext == '.svg':
                 i.rename(Path(p_img, new_name + ext))
-            elif ext == '.doc' or ext == '.docx' or ext == '.txt' or ext == '.pdf' or ext == '.xlsx' or ext == '.pptx': 
-                i.rename(Path(p_docs, new_name + ext))
+            elif ext == '.doc' or ext == '.docx' or ext == '.txt' or ext == '.pdf' or ext == '.xlsx' or ext == '.pptx':            
+                i.rename(Path(p_docs, new_name + ext))                
             elif ext == '.mp3' or ext == '.ogg' or ext == '.wav' or ext == '.amr': 
                 i.rename(Path(p_audio, new_name + ext))
             elif ext == '.avi' or ext == '.mp4' or ext == '.mov' or ext == '.mkv': 
@@ -74,10 +82,15 @@ def sort_files_folders(p):
                             and i.name != p_docs.name \
                                 and i.name != p_video.name:        
             shutil.rmtree(i)
+        
+
+    # with open(text_file, 'w') as fh:
+    #     fh.write(i.name+'\n')    
+            
                  
 sort_files_folders(p)
 
-#удалить пустые папки (непонятно) ++++++
-#записать инфо в файл (write)
-#загрузить в гитхаб (практика)
-#символы в имени файлах сделать как в задании сказано (replace)
+#удалить пустые папки (непонятно) +++++
+#записать инфо в файл.bin (write)
+#загрузить в гитхаб (практика) +++++
+#символы в имени файлах сделать как в задании сказано (replace) +++++
